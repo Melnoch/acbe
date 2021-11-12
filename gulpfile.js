c74c1a40cfd0d510ea56599cc975137f26d6b061
Melnoch <melnoch@hotmail.com>
2021-11-12 18:26:15 +0100

    [Level2][Lesson16] Add new lesson

--- gulpfile.js
+++ gulpfile.js
@@ -73,7 +73,8 @@ gulp.task('fileinclude', function() {
         'html/level2/lesson12.html',
         'html/level2/lesson13.html',
         'html/level2/lesson14.html',
-        'html/level2/lesson15.html'
+        'html/level2/lesson15.html',
+        'html/level2/lesson16.html'
     ])
         .pipe(fileinclude({
             prefix: '@@',
