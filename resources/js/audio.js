459fe58cb344e1a886de41ee4d1fd7bb102570de
Melnoch <melnoch@hotmail.com>
2021-12-11 12:28:25 +0100

    [TECH] Try iOS fix

--- resources/js/audio.js
+++ resources/js/audio.js
@@ -1,10 +1,11 @@
-let audio;
+let audio = new Audio();
+audio.muted = false;
+audio.autoplay = true;
+audio = document.getElementById('default-sound');
 function play(audioId) {
     if (audio != null) {
         audio.pause();
     }
-    audio = new Audio();
-    audio.play();
     audio = document.getElementById(audioId);
     audio.play();
 }
